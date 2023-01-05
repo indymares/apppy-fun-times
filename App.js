5c9f69fd9afc08c4b62cce289e88a4fe635e9def
Indy Mares <indianamares@icloud.com>
2023-01-04 21:21:24 -0800

    Added buttons and color and size generating

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const COLORS = ['red', 'blue', 'green', 'orange', 'yellow']
+const SIZES = [15, 32, 56, 72, 99]
 
 export default function App() {
+  const [color, setColor] = useState('purple')
+  const [size, setSize] = useState(28)
+
+  const getIndex = () => Math.floor(Math.random() * 5)
+  const getAnotherIndex = () => Math.floor(Math.random() * 5)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="dark" />
+      
+      <View style={styles.center}> 
+        <Text style={{fontSize: size, fontWeight: "bold", color: color}}>Hello!</Text>
+        <Text style={{fontSize: size, fontWeight: "bold", color: color}}>THIS IS AWESOME!</Text>
+      </View>
+      
+      <View>
+        <Button
+          title="Press me"
+          color="black"
+          onPress={() => {
+            setColor(COLORS[getIndex()])
+            setSize(SIZES[getAnotherIndex()])
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +39,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginHorizontal: 16,
+  },
+  center: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
